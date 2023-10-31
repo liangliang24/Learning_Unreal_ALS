da782bd8adcfbf4e68c177cfc94af62788e480d4
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2023-10-31 17:01:22 +0800

    New Project

---
+++ Source/LenrningForALS/LenrningForALSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LenrningForALSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALenrningForALSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALenrningForALSGameMode();
+};
+
+
+
