2394594d4d0e899076c44e61c433a7b3ba68c3c3
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2023-10-31 17:30:47 +0800

    Create ALSCharacter

---
+++ Source/LenrningForALS/Private/LALS_Base_Character.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+#include "LenrningForALS/Public/LALS_Base_Character.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ALALS_Base_Character::ALALS_Base_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+}
+
+
+// Called when the game starts or when spawned
+void ALALS_Base_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALALS_Base_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ALALS_Base_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/LenrningForALS/Public/LALS_Player_Controller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LALS_Player_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LENRNINGFORALS_API ALALS_Player_Controller : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
